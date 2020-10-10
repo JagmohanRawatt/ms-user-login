c043a79538d2b6c83b69481c0dcfd94893137d6b
Jagmohan Rawat <jagmohan.rawat2020@gmail.com>
2020-10-10 14:44:30 +0530

    Initial Commit

---
+++ src/main/java/com/jmcp/msuserlogin/MsUserLoginApplication.java
@@ -0,0 +1,13 @@
+package com.jmcp.msuserlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUserLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUserLoginApplication.class, args);
+	}
+
+}
